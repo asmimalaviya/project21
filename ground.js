3197767e2ce7553543dac420855a93060c0a373f
asmimalaviya <86877268+asmimalaviya@users.noreply.github.com>
2023-02-24 21:33:02 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,24 @@
+groundObj.display();
+groundObj=new ground(width/2,670,width,20);
+leftside= new ground(1100,600,20,120);
+
+Matter.Body.applyForce(body, position, force)
+
+function KeyPressed(){
+    if (KeyCode === UP_ARROW){
+
+    }
+}
+
+function KeyPressed(){
+    if (KeyCode ===  JUMP_ARROW){
+
+    }
+}
+
+function KeyPressed(){
+    if (KeyCode === FALL_ARROW){
+
+    }
+}
+
